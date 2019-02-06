1338150e14401cc7bc5c8d0cfeccebfa6af254d6
tterb <bstevensondev@gmail.com>
2019-02-05 18:16:39 -0800

    :building_construction: Integrate Contentful content-management

---
+++ src/templates/post.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Helmet from 'react-helmet'
+import get from 'lodash/get'
+import Img from 'gatsby-image'
+import PostLayout from '../components/PostLayout'
+import heroStyles from '../styles/hero.scss'
+import postStyles from '../styles/post.scss'
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = get(this.props, 'data.contentfulBlogPost')
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+
+    return (
+      <PostLayout location={this.props.location} >
+        <div style={{ background: '#fff' }}>
+          <Helmet title={`${post.title} | ${siteTitle}`} />
+          <div className={heroStyles.hero}>
+            <Img className={heroStyles.heroImage} alt={post.title} fluid={post.heroImage.fluid} />
+          </div>
+          <div className='wrapper' style={postStyles}>
+            <div className='post-header'>
+              <h1 className='post-title'>{post.title}</h1>
+              <p className='post-date' style={{ display: 'block' }}>
+                {post.publishDate}
+              </p>
+            </div>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: post.body.childMarkdownRemark.html,
+              }}
+            />
+          </div>
+        </div>
+      </PostLayout>
+    )
+  }
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishDate(formatString: "MMMM Do, YYYY")
+      heroImage {
+        fluid(maxWidth: 1180, background: "rgb:000000") {
+          ...GatsbyContentfulFluid_tracedSVG
+        }
+      }
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`
