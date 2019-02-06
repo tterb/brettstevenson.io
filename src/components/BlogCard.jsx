cde93188c1ce53f06285a4d3965b71155d2eede7
tterb <bstevensondev@gmail.com>
2019-02-05 18:27:55 -0800

    :building_construction: Integrate Contentful content-management

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import tw from 'tailwind.macro'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons'
 
 const Wrapper = styled.a`
   width: 100%;
@@ -23,20 +28,57 @@ const Title = styled.div`
   text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
 `
 
-const BlogCard = ({ topic, title, link, date, img, children }) => (
-  <div className='blog-card' bg={img}>
-    <span className='topic'>{children}</span>
-    <h2><a href={link}>{title}</a></h2>
-    <Text>{children}</Text>
-    <span className='date'>{date}</span>
-  </div>
-)
+const BgImage = styled(Image)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  height: 53vh; // or whatever
+  z-index: -1;
+  // Adjust image positioning (if image covers area with defined height) and add font-family for polyfill
+  & > img {
+    object-fit: cover !important; 
+    object-position: 0% 0% !important;
+  }
+  `
+
+class BlogCard extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+    location.href='/blog/'+this.props.post.slug
+  }
+  
+  render() {
+    return (
+      <div className='blog-card' style={{ position: `relative` }} onClick={this.handleClick}>
+        <Img alt="" 
+          css={{ top: 0, left: 0, right: 0, bottom: 0 }} 
+          style={{ position: `absolute` }} 
+          fluid={this.props.post.heroImage.fluid}/>
+        <div className='content-mask'>
+          <span className={`card-category ${this.props.post.category}`}>{this.props.post.category}</span>
+          <h2>
+            <Link to={`/blog/${this.props.post.slug}`}>{this.props.post.title}</Link>
+          </h2>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: this.props.post.description.childMarkdownRemark.html,
+            }}
+          />
+          <span className='card-date'>
+            <FontAwesomeIcon icon={faCalendarAlt} style={{ margin: `0 6px` }}/>
+            {this.props.post.publishDate}
+          </span>
+        </div>
+        <div class="horizontal"></div>
+      </div>
+    )
+  }
+}
 
 export default BlogCard
-
-BlogCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
-  bg: PropTypes.string.isRequired,
-}
