3798c8632933057f31c4b8b206f521b061baf831
tterb <bstevensondev@gmail.com>
2019-02-05 18:39:52 -0800

    :wrench: Update site configuration data

--- config/website.js
+++ config/website.js
@@ -3,20 +3,20 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Brett Stevenson', // Navigation and Site Title
+  siteTitleAlt: 'Brett', // Alternative Site title for SEO
+  siteTitleShort: 'Brett', // short_name for manifest
+  siteHeadline: 'Developer, Designer, Student', // Headline for schema.org JSONLD
+  siteUrl: 'https://tterb-gatsby.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'Developer, Designer, Student',
+  author: 'Brett Stevenson', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
-  ogLanguage: 'en_US', // Facebook Language
+  userTwitter: '@bstevensondev', // Twitter Username
+  // ogSiteName: 'cara', // Facebook Site Name
+  // ogLanguage: 'en_US', // Facebook Language
 
   // Manifest and Progress color
   themeColor: tailwind.colors.green,
