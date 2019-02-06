7572966f3073195c322b52ff0ed86067825c9c16
tterb <bstevensondev@gmail.com>
2019-02-05 18:25:30 -0800

    :wrench: Modify site Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,27 @@ const config = require('./config/website')
 
 const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
 
+let contentfulConfig
+
+// Load the Contentful config from the .contentful.json
+try {
+  contentfulConfig = require('./.contentful')
+} catch (_) {}
+
+// Overwrite the Contentful config with environment variables if they exist
+contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
+  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the delivery token need to be provided.'
+  )
+}
+
 module.exports = {
   /* General Information */
   siteMetadata: {
@@ -18,35 +39,43 @@ module.exports = {
         name: 'images',
       },
     },
+    'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: config.siteTitle,
-        short_name: config.siteTitleShort,
-        description: config.siteDescription,
-        start_url: config.pathPrefix,
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: 'standalone',
-        icons: [
-          {
-            src: '/favicons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/favicons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig,
     },
     /* Must be placed at the end */
     'gatsby-plugin-offline',
     'gatsby-plugin-netlify',
     'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-remark-prismjs`,
+      options: {
+        // Class prefix for <pre> tags containing syntax highlighting;
+        // defaults to 'language-' (eg <pre class="language-js">).
+        // If your site loads Prism into the browser at runtime,
+        // (eg for use with libraries like react-live),
+        // you may use this to prevent Prism from re-processing syntax.
+        // This is an uncommon use-case though;
+        // If you're unsure, it's best to use the default value.
+        classPrefix: "language-",
+        // This is used to allow setting a language for inline code
+        // (i.e. single backticks) by creating a separator.
+        // This separator is a string and will do no white-space
+        // stripping.
+        // A suggested value for English speakers is the non-ascii
+        // character '›'.
+        inlineCodeMarker: null,
+        // This lets you set up language aliases.  For example,
+        // setting this to '{ sh: "bash" }' will let you use
+        // the language "sh" which will highlight using the
+        // bash highlighter.
+        aliases: {},
+        showLineNumbers: false,
+        noInlineHighlight: false,
+      },
+    },
   ],
 }
