a95e2ebee89152c4557add4e86cae4af13e484a0
tterb <bstevensondev@gmail.com>
2019-03-03 20:33:44 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
-const config = require('./config/website')
-const dotenv = require('dotenv')
+const config = require(`./config/website`)
+const dotenv = require(`dotenv`)
 
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+const pathPrefix = config.pathPrefix === `/` ? `` : config.pathPrefix
 
 if (process.env.ENVIRONMENT !== "production") {
   dotenv.config();
@@ -13,7 +13,7 @@ let contentfulConfig
 
 // Load the Contentful config from the .contentful.json
 try {
-  contentfulConfig = require('./.contentful')
+  contentfulConfig = require(`./.contentful`)
 } catch (_) {}
 
 // Overwrite the Contentful config with environment variables if they exist
@@ -26,7 +26,7 @@ const { spaceId, accessToken } = contentfulConfig
 
 if (!spaceId || !accessToken) {
   throw new Error(
-    'Contentful spaceId and the delivery token need to be provided.'
+    `Contentful spaceId and the delivery token need to be provided.`
   )
 }
 
@@ -34,35 +34,47 @@ module.exports = {
   /* General Information */
   siteMetadata: {
     siteUrl: config.siteUrl + pathPrefix,
+    title: `Brett Stevenson`,
+    menuLinks: [
+      {
+        name: `Home`,
+        link: `/`
+      },
+      {
+        name: `Blog`,
+        link: `/blog`
+      },
+    ]
   },
   /* Plugins */
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/images/`,
-        name: 'images',
+        name: `images`,
       },
     },
     { 
-      resolve: 'gatsby-transformer-remark',
+      resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           `gatsby-remark-prismjs`,  
         ],
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
-      resolve: 'gatsby-source-contentful',
+      resolve: `gatsby-source-contentful`,
       options: contentfulConfig,
     },
     /* Must be placed at the end */
-    'gatsby-plugin-offline',
-    'gatsby-plugin-netlify',
-    'gatsby-plugin-sass',
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-transition-link`,
   ],
 }
