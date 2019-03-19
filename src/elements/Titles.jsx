a8dce6789fa9c0b51eef55843f019c49da93c74e
tterb <bstevensondev@gmail.com>
2019-03-19 02:23:38 -0700

    Improve element responsiveness and formatting

--- src/elements/Titles.jsx
+++ src/elements/Titles.jsx
@@ -10,7 +10,8 @@ export const Title = styled.h1`
 `
 
 export const BigTitle = styled.h1`
-  ${tw`sm:text-5xl md:text-6xl lg:text-7xl font-title text-white mt-4 mb-6 tracking-wide`};
+  ${tw`sm:text-5xl ml-1 md:text-6xl lg:text-7xl font-title text-white mt-4 mb-6 tracking-wide`};
+  font-size: 3em;
   font-weight: 800;
   letter-spacing: 0.025em;
   line-height: 1.1;
@@ -19,6 +20,6 @@ export const BigTitle = styled.h1`
 `
 
 export const Subtitle = styled.p`
-  ${tw`text-2xl lg:text-3xl font-sans text-white mt-8 ml-2 xxl:w-3/4`};
+  ${tw`text-2xl sm:mt-2 lg:text-3xl font-sans text-white lg:mt-8 ml-2 xxl:w-3/4`};
   text-shadow: 0 2px 15px rgba(0, 0, 0, 0.2);
 `
