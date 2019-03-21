ff1a348642cdbea9cc2f991f0b294c651d6a5a0b
tterb <bstevensondev@gmail.com>
2019-03-21 02:47:42 -0700

    Add blog tags pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,29 @@
 const Promise = require('bluebird')
 const path = require('path')
+const _ = require("lodash")
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
   return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/post.jsx')
+    const postTemplate = path.resolve('./src/templates/post.jsx')
+    const tagTemplate = path.resolve("src/templates/tags.jsx")
     resolve(
       graphql(
         `{
-          allContentfulBlogPost {
+          allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
             edges {
               node {
                 title
+                publishDate(formatString: "DD MMMM, YYYY")
                 slug
+                tags
+                description {
+                  childMarkdownRemark {
+                    html
+                    excerpt(pruneLength: 80)
+                  }
+                }
               }
             }
           }
@@ -25,17 +35,19 @@ exports.createPages = ({ graphql, actions }) => {
         }
         
         const posts = result.data.allContentfulBlogPost.edges
-        posts.forEach((post, index) => {
+        posts.forEach(( post, index) => {
           createPage({
             path: `/blog/${post.node.slug}/`,
-            component: blogPost,
+            component: postTemplate,
             context: {
-              slug: post.node.slug
+              slug: post.node.slug,
+              prev: index === 0 ? null : posts[index - 1],
+              next: index === result.length - 1 ? null : posts[index + 1],
             },
           })
         })
         
-        // Create blog-list pages
+        // Create blog pagination
         const postsPerPage = 4
         const numPages = Math.ceil(posts.length / postsPerPage)
         Array.from({ length: numPages }).forEach((_, i) => {
@@ -47,6 +59,28 @@ exports.createPages = ({ graphql, actions }) => {
               skip: i * postsPerPage,
               numPages,     
               currentPage: i + 1
+            },
+          })
+        })
+        
+        // Tag pages:
+        let tags = []
+        // Iterate through each post adding tags into `tags`
+        _.each(posts, edge => {
+          if (_.get(edge, "node.tags")) {
+            tags = tags.concat(edge.node.tags)
+          }
+        })
+        // Eliminate duplicate tags
+        tags = _.uniq(tags)
+
+        // Make tag pages
+        tags.forEach(tag => {
+          createPage({
+            path: `/tags/${_.kebabCase(tag)}/`,
+            component: tagTemplate,
+            context: { 
+              tag: tag,
             },
           })
         })
