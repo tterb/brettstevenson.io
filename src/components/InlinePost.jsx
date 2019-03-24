435ca5db19b6848c3edd722acd89f17b34360035
tterb <bstevensondev@gmail.com>
2019-03-23 21:37:16 -0700

    Modify inline blog-post contents

--- src/components/InlinePost.jsx
+++ src/components/InlinePost.jsx
@@ -1,14 +1,15 @@
-import React from "react"
+import React from 'react'
 import tw from 'tailwind.macro'
+import { colors } from '../../tailwind'
 import styled from 'styled-components'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
 const InlinePost = ({node}) => (
   <li key={node.slug}>
-    <AniLink cover bg="#23262b" duration={1} to={`blog/${node.slug}`}>
-      <span className="date">{node.publishDate}</span>
-      <span className="title">{node.title}</span>
-      <span className="desc">{node.description.childMarkdownRemark.excerpt}</span>
+    <AniLink cover bg={colors['blue-black']} duration={1} to={`blog/${node.slug}`}>
+      <span className='date'>{node.publishDate}</span>
+      <span className='title'>{node.title}</span>
+      <span className='desc'>{node.description.childMarkdownRemark.excerpt}</span>
     </AniLink>
   </li>
 )
