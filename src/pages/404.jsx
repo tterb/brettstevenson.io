994ca69a9c9fff32846f83ffe71ffe51d6a34835
tterb <bstevensondev@gmail.com>
2019-03-30 19:54:21 -0700

    :warning: Add site 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import tw from 'tailwind.macro'
+import { colors, accent } from '../../tailwind'
+import styled from 'styled-components'
+import { Parallax } from 'react-spring/renderprops-addons'
+import Typed from 'react-typed';
+// Components
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+// Elements
+import Content from '../elements/Content'
+import { BigTitle } from '../elements/Titles'
+// Views
+import Footer from '../views/Footer'
+// Styles
+import '../styles/404.scss'
+
+
+const PageTitle = styled(BigTitle)`
+  ${tw`sm:text-6xl ml-1 md:text-8xl lg:text-9xl font-title text-center font-extrabold mt-0 ml-0`};
+  font-size: 8rem;
+  text-shadow: 0 2.5px 6px rgba(0,0,0,0.4) !important;
+`
+
+const LayoutStyle = {
+  background: 'linear-gradient(to bottom, #23262b, #161719)',
+}
+
+const typedStyle = {
+  color: `rgba(255,255,255,0.75)`,
+};
+
+class FourOhFour extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {value:''}
+    this.handleChange = this.handleChange.bind(this);
+    this.keyPress = this.keyPress.bind(this);
+  }
+  
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  keyPress(e) {
+    if(e.keyCode == 13) {
+      e.preventDefault();
+      console.log('value', e.target.value);
+      if(e.target.value.toLowerCase() == 'y')
+        window.location.href = '../';
+    }
+  }
+  
+  render() {
+    return (
+      <>
+        <Layout style={`background: linear-gradient(to bottom, #23262b, #161719)`}/>
+        <Parallax pages={1.25}>
+        <Content offset={0} factor={0.375} speed={0.6}>
+          <Header offset={0} factor={0.4} speed={0.6}>
+            <PageTitle>404<span className='accent'>.</span></PageTitle>
+          </Header>
+          </Content>
+          <Content offset={0.325} factor={0.5} speed={0.5} style={`padding: 14rem !important`}>
+            <div class='terminal'>
+              <div class='window-title'>
+                <div class='win-buttons'>
+                  <a id='close' class='term-button' href='../'></a>
+                  <a id='min' class='term-button' href=''></a>
+                  <a id='max' class='term-button' href=''></a>
+                </div>
+                <span class='title'>404</span>
+              </div>
+              <div class='message'>
+                <span class='prompt'><span class='user'>root</span><span class='at'>@</span><span class='path'>~/404</span>><span class='caret'>$</span> </span>
+                  <Typed style={typedStyle} strings={[
+                      "Oops! <br/>" + "><span class='caret'>$</span> You might have mis-typed the URL, <br/> ^1" + "><span class='caret'>$</span> or maybe the page has been moved. <br/> ^500" + "><span class='caret'>$</span> Anyways, there is nothing to see here... <br/> ^500" + "><span class='caret'>$</span> Would you like to return to the <a href='../'>homepage</a> (Y/N)?"
+                      ]}
+                      cursorChar={'█'} showCursor={false}
+                      autoInsertCss={true} loop={false}
+                      typeSpeed={10} backSpeed={30}
+                      smartBackspace>
+                  </Typed>
+                  <form>&gt;<span class='caret'>$</span>
+                  <input id='term-input' type='text' 
+                  value={this.state.value} onKeyDown={this.keyPress}
+                  onChange={this.handleChange} autoFocus />
+                </form>
+              </div>
+            </div>
+          </Content>
+          <Footer offset={0.95} factor={0.5} speed={0.5} />
+        </Parallax>
+      </>
+    )
+  }
+}
+
+export default FourOhFour
