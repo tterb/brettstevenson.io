c886d0061ac0b0fef79cf0477e77dea212b3ea3a
tterb <bstevensondev@gmail.com>
2019-04-12 22:51:17 -0700

    Modify page header formatting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,15 @@ import { ParallaxLayer } from 'react-spring/renderprops-addons'
 const HeaderContent = styled(ParallaxLayer)`
   ${tw`p-2 px-8 md:p-12 lg:p-16 justify-center items-center flex z-50`};
   @media (min-width: 400px) {
-    height: 35vh !important;
+    height: 40vh !important;
   }
   @media (min-width: 600px) {
-    height: 40vh !important;
+    height: 45vh !important;
   }
 `
 
 const Wrapper = styled.div`
-  ${tw`w-full xl:w-5/6`};
-  position: relative;
+  ${tw`relative w-full mt-8 xl:w-5/6`};
 `
 
 const Header = ({ children, offset, speed }) => (
