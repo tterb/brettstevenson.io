c9b23d9ba26becaeb61b1215f5377a58da7b97cf
tterb <bstevensondev@gmail.com>
2019-04-12 22:09:36 -0700

    :card_index: Modify blog-post card styling

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,36 +1,40 @@
 import React from 'react'
 import Image from 'gatsby-image'
+import _ from 'lodash'
 import tw from 'tailwind.macro'
-import { colors } from '../../tailwind'
 import styled from 'styled-components'
-import kebabCase from 'lodash/kebabCase'
-import PageLink from './PageLink';
+import PageLink from './PageLink'
 // FontAwesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons'
 
-const Wrapper = styled.a`
-  width: 100%;
-  ${tw`shadow-lg relative no-underline rounded-lg px-8 py-8 md:py-16 text-white`};
-  background: ${props => props.bg};
-  transition: transform 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-  &:hover {
-    transform: translateY(-5px);
-  }
+const Wrapper = styled.div`
+  ${tw`relative inline-block h-full py-3 px-0 pl-10 cursor-pointer overflow-hidden`}
+  background: rgba(255,255,255,0.8);
+  top: 0px;
+  width: 65%;
+  border-top-left-radius: 9px;
+  border-bottom-left-radius: 9px;
+  z-index: 5;
 `
-const Title = styled.div`
-  ${tw`text-white uppercase text-2xl md:text-3xl xl:text-4xl tracking-wide font-sans pt-8`};
-  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+
+const Card = styled.div`
+  ${tw`block relative bg-center bg-no-repeat w-full float-left cursor-pointer`}
+  background-size: cover;
+  border-radius: 9px;
 `
-const Text = styled.div`
-  ${tw`opacity-75 font-sans text-sm md:text-base`};
-  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+
+const Angle = styled.div`
+  ${tw`relative inline-block w-1/5 h-full overflow-hidden`}
+  background: linear-gradient(to top right, rgba(255,255,255,0.8) 50%, transparent 0);
+  top: 0;
+  right: 0;
 `
+
 const BgImage = styled(Image)`
-  position: absolute;
+  ${tw`absolute min-w-full`}
   top: 0;
   left: 0;
-  min-width: 100%;
   height: 53vh; // or whatever
   z-index: -1;
   // Adjust image positioning (if image covers area with defined height) and add font-family for polyfill
@@ -38,13 +42,23 @@ const BgImage = styled(Image)`
     object-fit: cover !important; 
     object-position: 0% 0% !important;
   }
-  `
+`
+
+const Title = styled.div`
+  ${tw`text-white uppercase text-2xl md:text-3xl xl:text-4xl tracking-wide font-title pt-8`};
+  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+`
+
+const Text = styled.div`
+  ${tw`opacity-75 font-sans text-sm md:text-base`};
+  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+`
 
 class BlogCard extends React.Component {
   
   constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
   }
   
   handleClick() {
@@ -53,15 +67,15 @@ class BlogCard extends React.Component {
   
   render() {
     return (
-      <div className='blog-card' style={{ position: `relative` }} onClick={this.handleClick}>
+      <Card className='blog-card' onClick={this.handleClick}>
         <Image alt={this.props.post.title}
           css={{ top: 0, left: 0, right: 0, bottom: 0 }} 
           style={{ position: `absolute` }} 
           fixed={this.props.post.heroImage.fixed}/>
-        <div className='content-mask'>
-          <span className={`card-category ${kebabCase(this.props.post.category.toString().toLowerCase())}`}>{this.props.post.category}</span>
+        <Wrapper className='content-mask'>
+          <span className={`card-category ${_.kebabCase(this.props.post.category.toString().toLowerCase())}`}>{this.props.post.category}</span>
           <div className='text-container'>
-            <h2>
+            <h2 className='card-title'>
               <PageLink to={`/blog/${this.props.post.slug}`}>{this.props.post.title}</PageLink>
             </h2>
             <p dangerouslySetInnerHTML={{ __html: this.props.post.description.childMarkdownRemark.html }}
@@ -71,9 +85,9 @@ class BlogCard extends React.Component {
               {this.props.post.publishDate}
             </span>
           </div>
-        </div>
-        <div className='horizontal'></div>
-      </div>
+        </Wrapper>
+        <Angle />
+      </Card>
     )
   }
 }
