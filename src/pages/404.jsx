5cc9f26873f7454dfe5634057fd6faa2933b0a2b
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-05-17 23:01:14 -0700

    :art: Modify 404 page styling and formatting

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -22,6 +22,61 @@ const PageTitle = styled(BigTitle)`
   text-shadow: 0 2.5px 6px rgba(0,0,0,0.4) !important;
 `
 
+const TermTitle = styled.span`
+  ${tw`relative inline-block font-normal tracking-wide text-center align-middle`}
+  color: rgba(0,0,0,0.65);
+  top: 0.5px;
+  left: -45px;
+`
+
+const TermBody = styled.div`
+  ${tw`relative text-sm font-medium leading-normal m-auto p-0 pt-4 z-999`}
+  font-family: Monaco, Consolas, 'Ubuntu Mono', monospace;
+  color: rgba(255,255,255,0.75);
+  width: 94%;
+  span {
+    ${tw`font-light leading-tight mb-4`}
+  }
+  a {
+    border-bottom: 1px dotted rgba(255,255,255,0.75);
+  }
+`
+
+const Prompt = styled.span`
+  ${tw`text-base pr-1`}
+  span {
+    letter-spacing: 0.002em;
+    padding-right: 2px;
+  }
+  .user {
+    color: #ea7e7d;
+  }
+  .at {
+    color: #ebdb84;
+  }
+  .path {
+    color: #7ecdcb;
+  }
+`
+
+const TermInput = styled.form`
+  ${tw`text-base mt-1`}
+  input {
+    ${tw`inline-block w-4/5 h-4 border-none rounded-none py-0 px-2 shadow-none outline-none`}
+    background: transparent;
+    caret-color: #50cd70;
+    &:focus {
+      ${tw`border-none outline-none`}
+      background: transparent !important;
+      box-shadow: none !important;
+    }
+  }
+`
+
+const PageHeader = styled(Header)`
+  padding-top: 2rem !important;
+`
+
 const LayoutStyle = {
   background: 'linear-gradient(to bottom, #23262b, #161719)',
 }
@@ -55,39 +110,45 @@ class FourOhFour extends React.Component {
   render() {
     return (
       <>
-        <Layout style={`background: linear-gradient(to bottom, #23262b, #161719)`}/>
+        <Layout style={{background: `linear-gradient(to bottom, #23262b, #161719)`}}/>
         <Parallax pages={1.25}>
         <Content offset={0} factor={0.375} speed={0.6}>
-          <Header offset={0} factor={0.4} speed={0.6}>
+          <Header offset={0} factor={0.4} speed={0.6} style={{ paddingTop: `0` }}>
             <PageTitle>404<span className='accent'>.</span></PageTitle>
           </Header>
           </Content>
-          <Content offset={0.325} factor={0.5} speed={0.5} style={`padding: 14rem !important`}>
+          <Content offset={0.325} factor={0.5} speed={0.5} style={{padding: `14rem !important`}}>
             <div className='terminal'>
               <div className='window-title'>
                 <div className='win-buttons'>
                   <a id='close' className='term-button' href='../'></a>
-                  <a id='min' className='term-button' href=''></a>
-                  <a id='max' className='term-button' href=''></a>
+                  <a id='min' className='term-button' href='#'></a>
+                  <a id='max' className='term-button' href='#'></a>
                 </div>
-                <span className='title'>404</span>
+                <TermTitle>404</TermTitle>
               </div>
-              <div className='message'>
-                <span className='prompt'><span className='user'>root</span><span className='at'>@</span><span className='path'>~/404</span>><span className='caret'>$</span> </span>
-                  <Typed style={typedStyle} strings={[
-                      "Oops! <br/>" + "><span className='caret'>$</span> You might have mis-typed the URL, <br/> ^1" + "><span className='caret'>$</span> or maybe the page has been moved. <br/> ^500" + "><span className='caret'>$</span> Anyways, there is nothing to see here... <br/> ^500" + "><span className='caret'>$</span> Would you like to return to the <a href='../'>homepage</a> (Y/N)?"
-                      ]}
-                      cursorChar={'█'} showCursor={false}
-                      autoInsertCss={true} loop={false}
-                      typeSpeed={10} backSpeed={30}
-                      smartBackspace>
-                  </Typed>
-                  <form>&gt;<span className='caret'>$</span>
+              <TermBody className='message'>
+                <Prompt className='prompt'>
+                  <span className='user'>root</span>
+                  <span className='at'>@</span>
+                  <span className='path'>~/404</span>
+                  <span>&gt;$</span>
+                </Prompt>
+                <Typed style={typedStyle} strings={[
+                    "Oops! <br/>" + "><span className='caret'>$</span> You might have mis-typed the URL, <br/> ^1" + "><span className='caret'>$</span> or maybe the page has been moved. <br/> ^500" + "><span className='caret'>$</span> Anyways, there is nothing to see here... <br/> ^500" + "><span className='caret'>$</span> Would you like to return to the <a href='../'>homepage</a> (Y/N)?"
+                    ]}
+                    cursorChar={'█'} showCursor={false}
+                    autoInsertCss={true} loop={false}
+                    typeSpeed={10} backSpeed={30}
+                    smartBackspace>
+                </Typed>
+                <TermInput>&gt;<span className='caret'>$</span>
                   <input id='term-input' type='text' 
-                  value={this.state.value} onKeyDown={this.keyPress}
-                  onChange={this.handleChange} autoFocus />
-                </form>
-              </div>
+                    value={this.state.value} 
+                    onKeyDown={this.keyPress}
+                    onChange={this.handleChange} autoFocus/>
+                </TermInput>
+              </TermBody>
             </div>
           </Content>
           <Footer offset={0.95} factor={0.5} speed={0.5} />
