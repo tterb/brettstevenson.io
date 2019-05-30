9b977ad8b41338278e9c9298af38f30d53a6b221
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-05-30 03:17:23 -0700

    Add default props for site footer

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -86,10 +86,10 @@ const GatsbyIcon = () => (
   <svg xmlns='https://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 32 32' style={iconStyle}><path d='M15.9554.0891A15.9555,15.9555,0,1,0,31.9109,16.0446,15.9555,15.9555,0,0,0,15.9554.0891ZM3.4284,16.2109,15.7891,28.5716A12.528,12.528,0,0,1,3.4284,16.2109ZM18.76,28.2584,3.7416,13.24A12.531,12.531,0,0,1,26.085,8.671L24.35,10.2016a10.23,10.23,0,0,0-18.0141,2.36L19.438,25.6638a10.249,10.249,0,0,0,6.4484-7.1646H20.4557V16.0446h8.0289A12.5338,12.5338,0,0,1,18.76,28.2584Z' transform='translate(0 -0.0891)' fill='#9BABB4'/></svg>
 )
 
-const Footer = ({ offset }) => (
+const Footer = ({ offset, speed }) => (
   <>
-    <Content offset={`${offset+0.025}`} speed={0} factor={0.2} style={{ zIndex: `99999` }}>
-      <DividerStatic className='footer-bg' fill={colors['blue-grey']} speed={0.35} offset={`${offset}`} style={{ zIndex: `999` }} factor={0.2}>
+    <Content offset={offset+0.025} speed={0} factor={0.2} style={{ zIndex: `99999` }}>
+      <DividerStatic className='footer-bg' fill={colors['blue-grey']} speed={speed} offset={offset} style={{ zIndex: `999` }} factor={0.2}>
         <WaveWrapper style={{ height: `100%`, top: `2rem`, left: `0` }}>
           <InnerWave>
             <svg xmlns='https://www.w3.org/2000/svg' viewBox='0 0 800 338.05' preserveAspectRatio='none'>
@@ -117,6 +117,11 @@ const Footer = ({ offset }) => (
 
 export default Footer
 
+Footer.defaultProps = {
+  speed: 0.35
+}
+
 Footer.propTypes = {
   offset: PropTypes.number.isRequired,
+  speed: PropTypes.number
 }
