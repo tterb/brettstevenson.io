39ee47ff5195c3995053af94d9c700d53fee53a6
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-05-30 03:14:09 -0700

    Improve blog-card component styling and formatting

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -11,7 +11,6 @@ import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons'
 const Wrapper = styled.div`
   ${tw`relative inline-block h-full pin-t pin-l py-3 px-0 pl-10 cursor-pointer overflow-hidden z-5`}
   background: rgba(255,255,255,0.8);
-  /* top: 0px; */
   width: 65%;
   border-top-left-radius: 9px;
   border-bottom-left-radius: 9px;
@@ -39,14 +38,48 @@ const BgImage = styled(Image)`
   }
 `
 
-const Title = styled.div`
-  ${tw`text-white uppercase text-2xl md:text-3xl xl:text-4xl tracking-wide font-title pt-8`};
-  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+const CardContent = styled.div`
+  margin-top: 0.35rem;
 `
 
-const Text = styled.div`
-  ${tw`opacity-75 font-sans text-sm md:text-base`};
-  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+const Category = styled.span`
+  ${tw`inline-block text-center tracking-wide mt-4 mb-6 mx-0 cursor-pointer z-10`}
+  color: rgba(255,255,255,0.9);
+  text-shadow: 0 1px 0.25px rgba(0,0,0,0.5);
+  min-width: 45px;
+  border-radius: 5px;
+  transition: all 400ms ease-in-out;
+  padding: 6px 8px;
+`
+
+const CardTitle = styled.h2`
+  ${tw`font-title font-bold leading-tight mt-0 pb-1`}
+  color: rgba(0,0,0,0.8);
+  font-size: 2.7rem;
+  /* border-bottom: 2px solid rgba(0,0,0,0.125); */
+  border-bottom: 2px solid rgba(255,255,255,0.5);
+  margin-bottom: 0.35em;
+  a {
+    ${tw`no-underline`}
+    color: rgba(0,0,0,0.85);
+  }
+`
+
+const Text = styled.p`
+  ${tw`tracking-wide leading-normal w-full mb-4 pb-1 cursor-pointer z-10`}
+  color: rgba(0,0,0,0.65);
+  font-size: 1.05rem;
+  margin-top: 0.3em;
+  padding-left: 2px; 
+`
+
+const Date = styled.span`
+  ${tw`absolute text-base`}
+  color: rgba(0,0,0,0.55);
+  bottom: 3rem;
+  svg {
+    margin: 0 10px 0 6px;
+  }
 `
 
 class BlogCard extends React.Component {
@@ -65,18 +98,18 @@ class BlogCard extends React.Component {
       <Card className='blog-card' onClick={this.handleClick}>
         <BgImage alt={this.props.post.title} fixed={this.props.post.heroImage.fixed} />
         <Wrapper className='content-mask'>
-          <span className={`card-category ${_.kebabCase(this.props.post.category.toString().toLowerCase())}`}>{this.props.post.category}</span>
-          <div className='text-container'>
-            <h2 className='card-title'>
+          <Category className={`card-category ${_.kebabCase(this.props.post.category.toString().toLowerCase())}`}>{this.props.post.category}</Category>
+          <CardContent>
+            <CardTitle>
               <PageLink to={`/blog/${this.props.post.slug}`}>{this.props.post.title}</PageLink>
-            </h2>
-            <p dangerouslySetInnerHTML={{ __html: this.props.post.description.childMarkdownRemark.html }}
+            </CardTitle>
+            <Text dangerouslySetInnerHTML={{ __html: this.props.post.description.childMarkdownRemark.html }}
             />
-            <span className='card-date'>
-              <FontAwesomeIcon icon={faCalendarAlt} style={{ margin: `0 6px` }}/>
+            <Date>
+              <FontAwesomeIcon icon={faCalendarAlt} />
               {this.props.post.publishDate}
-            </span>
-          </div>
+            </Date>
+          </CardContent>
         </Wrapper>
         <Angle />
       </Card>
