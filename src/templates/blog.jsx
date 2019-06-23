4386e4de0658f84ede526c3a4694ed6a00d16057
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-06-22 21:12:59 -0700

    Modify blog page template

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -6,7 +6,7 @@ import BlogLayout from '../components/BlogLayout'
 class Blog extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = _.get(this, 'props.data.allContentfulBlogPost.edges')
+    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
     const { currentPage, numPages, count } = this.props.pageContext
     return (
       <>
