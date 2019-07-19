636c7b21fb87a342ff5e889fab9dc8bf6e987fd7
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-07-18 21:14:28 -0700

    Modify site page layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import SEO from './SEO'
+// Components
+import DevToolsMsg from './DevToolsMsg'
 // Styles
 import GlobalStyle from '../styles/global'
 import 'typeface-titillium-web'
 
-const Layout = () => (
-  <>
-    <SEO />
-    <GlobalStyle />
-  </>
-)
+const Layout = () => {
+  if (typeof window !== "undefined") {
+    require("smooth-scroll")('a[href*="#"]', {
+      speed: 400,
+      easing: 'easeInOutQuad',
+    })
+  }
+  return (
+    <>
+      <SEO />
+      <DevToolsMsg />
+      <GlobalStyle />
+    </>
+  )
+}
 
 export default Layout
