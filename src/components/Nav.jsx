e1b8dff65a2e24fdd9c15dd5ff5a8ed0c6580bd6
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-07-18 21:06:16 -0700

    Add supplementary mobile navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,7 +10,7 @@ import PageLink from './PageLink'
 import Logo from './Logo'
 
 const Wrapper = styled.div`
-  ${tw`relative block font-title h-4 pin-t pin-x z-999`}
+  ${tw`relative block font-title h-0 pin-t pin-x z-999`}
   transform: translate3D(0, 0, 0);
 `
 
@@ -59,16 +59,134 @@ const Navbar = styled.div`
   }
 `
 
-const Nav = ({ logo }) => (
-  <StaticQuery query={menuQuery} 
-    render={data => (
-      <>
-      <Wrapper className='nav-wrapper'>
-        <Fade top delay={250}>
-          <MenuContainer>
-            {logo ? <Logo className='logo-container' link={data.site.siteMetadata.menuLinks[0].link} /> : ''}
-            <Navbar>
-              <Menu className='menu'>
+const MenuPanel = styled.div`
+  ${tw`fixed w-1/2 pin-t pin-r px-6 py-10`}
+  position: fixed;
+  background: ${colors['background-alt']};
+  height: 300vh;
+  top: 0;
+  box-shadow: 0 0 10px rgba(0,0,0,0.7);
+  transform: translateX(20rem);
+  transition: transform 500ms ease;
+  z-index: -1;
+  &.active {
+    transform: translateX(0)
+  }
+  li {
+    ${tw`w-9/10 mt-3 p-0 py-3`}
+    border-bottom: 1px solid rgba(255,255,255,0.1);
+  }
+`
+
+const MenuButton = styled.span`
+  ${tw`absolute text-3xl ml-auto overflow-visible z-9999`}
+  color: rgba(255,255,255,0.75);
+  width: 35px;
+  height: 35px;
+  top: 1rem;
+  right: 1rem;
+  &.active {
+    position: fixed;
+    top: 0.25rem;
+    span {
+      background: transparent;
+      top: 1rem;
+    }
+    span::before {
+      top: 1rem;
+      transform-origin: center;
+      transform: rotate(-45deg);
+    }
+    span::after {
+      top: 1rem;
+      transform-origin: center;
+      transform: rotate(45deg);
+    }
+  }
+`
+
+const Icon = styled.span`
+  ${tw`absolute block w-full overflow-visible z-9999`}
+  background: #fff;
+  height: 3px;
+  top: 1rem;
+  border-radius: 20px;
+  transition: position 300ms ease-in-out, transform 500ms ease;
+  &::before, &::after {
+    ${tw`absolute block w-7/10 overflow-visible z-9999`}
+    background: #fff;
+    content: '';
+    height: 3px;
+    top: 1rem;
+    border-radius: 20px;
+    transition: position 300ms ease-in-out, transform 500ms ease;
+  }
+  &::before {
+    top: -8px;
+  }
+  &::after {
+    top: 8px;
+  }
+`
+
+class Nav extends React.Component {
+  
+  state = {
+    panel: false,
+  }
+  
+  togglePanel = () => {
+    if(this.state.panel)
+      this.setState({ panel: false }) 
+    else
+      this.setState({ panel: true })
+  }
+  
+  isPanelVisible = () => {
+    return this.state.panel ? 'active' : ''
+  }
+  
+  render() {
+    const logo = this.props.logo
+    const mobile = this.props.mobile
+    const panel = this.state.panel
+    return (
+      <StaticQuery query={menuQuery} 
+        render={data => (
+          <>
+          <Wrapper className='nav-wrapper'>
+            <Fade top delay={250}>
+              <MenuContainer>
+                {logo ? <Logo className='logo-container' link={data.site.siteMetadata.menuLinks[0].link} /> : null}
+                { mobile ?
+                  <MenuButton className={`menu-button ${this.isPanelVisible()}`} onClick={this.togglePanel}>
+                    <Icon />
+                  </MenuButton>
+                  :
+                  <Navbar>
+                    <Menu className='menu'>
+                      {data.site.siteMetadata.menuLinks.map((item, i) => {
+                        if(item.external) {
+                          return (
+                            <MenuItem className='menu-item external' key={item.name}>
+                              <a href={item.link} target='_blank'>{item.name}</a>
+                            </MenuItem>
+                          )
+                        } else {
+                          return (
+                            <MenuItem className='menu-item' key={item.name}>
+                              <PageLink to={item.link}>{item.name}</PageLink>
+                            </MenuItem>
+                          )
+                        }
+                      })}
+                    </Menu>
+                  </Navbar>
+                }
+              </MenuContainer>
+            </Fade>
+            { mobile ?
+              <MenuPanel className={`${this.isPanelVisible()}`}>
                 {data.site.siteMetadata.menuLinks.map((item, i) => {
                   if(item.external) {
                     return (
@@ -84,21 +202,22 @@ const Nav = ({ logo }) => (
                     )
                   }
                 })}
-              </Menu>
-            </Navbar>
-          </MenuContainer>
-        </Fade>
-      </Wrapper>
-      </>
-    )}
-  />
-)
+              </MenuPanel> : null }
+          </Wrapper>
+          </>
+        )}
+      />
+    )
+  }
+}
 
 Nav.defaultProps = {
   logo: true,
+  mobile: false,
 }
 Nav.propTypes = {
-  logo: PropTypes.bool
+  logo: PropTypes.bool,
+  mobile: PropTypes.bool,
 }
 
 const menuQuery = graphql`
