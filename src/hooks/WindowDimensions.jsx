4e628ed0ab2aa9fc55a59b2c2f2192a8ab90405d
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-07-18 23:24:38 -0700

    Add additional hook functionality

--- src/hooks/WindowDimensions.jsx
+++ src/hooks/WindowDimensions.jsx
@@ -1,11 +1,21 @@
 import { useState, useEffect } from 'react';
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window
-  return {
-    width,
-    height
+  if (typeof window !== 'undefined') {
+    const { innerWidth: width, innerHeight: height } = window
+    return {
+      width,
+      height
+    }
   }
+}
+
+function getMobileState() {
+  if (typeof window !== 'undefined') {
+    const { width, height } = getWindowDimensions()
+    return (width <= 500)
+  }
+  return false
 }
 
 export default function useWindowDimensions() {
@@ -21,4 +31,20 @@ export default function useWindowDimensions() {
   }, [])
 
   return windowDimensions
+}
+
+
+export function isMobile() {
+  const [mobileState, setMobileState] = useState(getMobileState())
+
+  useEffect(() => {
+    function handleResize() {
+      setMobileState(getMobileState())
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
+
+  return mobileState
 }
