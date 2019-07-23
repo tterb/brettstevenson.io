491ad967b43aaf502bfc4e6d6e4de2a42942f562
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-07-22 23:38:15 -0700

    Optimize global styles

--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -1,28 +1,33 @@
 import { createGlobalStyle } from 'styled-components'
-import tw from 'tailwind.macro'
-import { accent } from '../../tailwind'
+import { colors, accent } from '../../tailwind'
 
 const GlobalStyle = createGlobalStyle`
   *, *:before, *:after {
     box-sizing: inherit;
   }
   html {
-    background-color: #161719;
+    background-color: ${colors['blue-black']};
     text-rendering: optimizeLegibility;
     overflow-x: hidden;
     box-sizing: border-box;
     -ms-overflow-style: scrollbar;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
   html, body {
-    ${tw`font-title w-full h-full m-0 p-0`}
+    font-family: 'Product Sans', 'Titillium Web', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', sans-serif;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
   }
   a {
-    ${tw`no-underline border-none cursor-pointer`}
     color: rgba(255,255,255,0.8);
+    text-decoration: none;
+    border: none;
     transition: all 300ms ease-in-out;
+    cursor: pointer;
     &:hover,
     &.active a {
       color: ${accent};
