40df0efde9bbd17711e975684a34f5651ba82d18
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-07-22 23:38:54 -0700

    Update site layout imports

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+// Componemts
 import SEO from './SEO'
 // Styles
 import GlobalStyle from '../styles/global'
-import 'typeface-titillium-web'
 
 const Layout = () => {
+  require('typeface-titillium-web')
   if (typeof window !== "undefined") {
     require("smooth-scroll")('a[href*="#"]', {
       speed: 400,
