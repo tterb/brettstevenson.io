af9693b5425e54ac3901ee5696c02498cc015a0e
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-08-18 21:39:49 -0700

    :art: Modify global site styling

--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -5,22 +5,20 @@ const GlobalStyle = createGlobalStyle`
   *, *:before, *:after {
     box-sizing: inherit;
   }
-  html {
+  body {
     background-color: ${colors['blue-black']};
+    font-family: 'Product Sans', 'Titillium Web', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', sans-serif;
     text-rendering: optimizeLegibility;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
     overflow-x: hidden;
     box-sizing: border-box;
     -ms-overflow-style: scrollbar;
     -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-  }
-  html, body {
-    font-family: 'Product Sans', 'Titillium Web', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', sans-serif;
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
   }
 `
 
