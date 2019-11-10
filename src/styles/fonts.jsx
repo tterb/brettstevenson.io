aa30b4de21f2a728f651926ca48a592172b7a41e
Brett Stevenson <bstevensondev@gmail.com>
2019-11-09 22:22:49 -0800

    Modify site typeface handling

---
+++ src/styles/fonts.jsx
@@ -0,0 +1,67 @@
+import TTNormsBlackWOFF from '../fonts/TTNorms-Black.woff'
+import TTNormsBlackWOFF2 from '../fonts/TTNorms-Black.woff2'
+
+import TTNormsHeavyWOFF from '../fonts/TTNorms-Heavy.woff'
+import TTNormsHeavyWOFF2 from '../fonts/TTNorms-Heavy.woff2'
+import TTNormsHeavyItalicWOFF from '../fonts/TTNorms-HeavyItalic.woff'
+import TTNormsHeavyItalicWOFF2 from '../fonts/TTNorms-HeavyItalic.woff2'
+
+import TTNormsExtraBoldWOFF from '../fonts/TTNorms-ExtraBold.woff'
+import TTNormsExtraBoldWOFF2 from '../fonts/TTNorms-ExtraBold.woff2'
+import TTNormsExtraBoldItalicWOFF from '../fonts/TTNorms-ExtraBoldItalic.woff'
+import TTNormsExtraBoldItalicWOFF2 from '../fonts/TTNorms-ExtraBoldItalic.woff2'
+
+import TTNormsBoldWOFF from '../fonts/TTNorms-Bold.woff'
+import TTNormsBoldWOFF2 from '../fonts/TTNorms-Bold.woff2'
+import TTNormsBoldItalicWOFF from '../fonts/TTNorms-BoldItalic.woff'
+import TTNormsBoldItalicWOFF2 from '../fonts/TTNorms-BoldItalic.woff2'
+
+import TTNormsMediumWOFF from '../fonts/TTNorms-Medium.woff'
+import TTNormsMediumWOFF2 from '../fonts/TTNorms-Medium.woff2'
+import TTNormsMediumItalicWOFF from '../fonts/TTNorms-MediumItalic.woff'
+import TTNormsMediumItalicWOFF2 from '../fonts/TTNorms-MediumItalic.woff2'
+
+import TTNormsRegularWOFF from '../fonts/TTNorms-Regular.woff'
+import TTNormsRegularWOFF2 from '../fonts/TTNorms-Regular.woff2'
+import TTNormsItalicWOFF from '../fonts/TTNorms-Italic.woff'
+import TTNormsItalicWOFF2 from '../fonts/TTNorms-Italic.woff2'
+
+import TTNormsLightWOFF from '../fonts/TTNorms-Light.woff'
+import TTNormsLightWOFF2 from '../fonts/TTNorms-Light.woff2'
+import TTNormsLightItalicWOFF from '../fonts/TTNorms-LightItalic.woff'
+import TTNormsLightItalicWOFF2 from '../fonts/TTNorms-LightItalic.woff2'
+
+export default {
+  TTNormsBlackWOFF2,
+  TTNormsBlackWOFF,
+
+  TTNormsHeavyWOFF2,
+  TTNormsHeavyWOFF,
+  TTNormsHeavyItalicWOFF2,
+  TTNormsHeavyItalicWOFF,
+
+  TTNormsExtraBoldWOFF2,
+  TTNormsExtraBoldWOFF,
+  TTNormsExtraBoldItalicWOFF2,
+  TTNormsExtraBoldItalicWOFF,
+
+  TTNormsBoldWOFF2,
+  TTNormsBoldWOFF,
+  TTNormsBoldItalicWOFF2,
+  TTNormsBoldItalicWOFF,
+
+  TTNormsMediumWOFF2,
+  TTNormsMediumWOFF,
+  TTNormsMediumItalicWOFF2,
+  TTNormsMediumItalicWOFF,
+
+  TTNormsRegularWOFF2,
+  TTNormsRegularWOFF,
+  TTNormsItalicWOFF2,
+  TTNormsItalicWOFF,
+
+  TTNormsLightWOFF2,
+  TTNormsLightWOFF,
+  TTNormsLightItalicWOFF2,
+  TTNormsLightItalicWOFF,
+}
--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -1,13 +1,121 @@
 import { createGlobalStyle } from 'styled-components'
-import { colors, accent } from '../../tailwind'
+import { colors } from '../../tailwind'
+import fonts from './fonts.jsx'
 
 const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: normal;
+    font-weight: 300;
+    font-display: auto;
+    src: local('TTNorms Light'), local('TTNorms-Light'), url(${fonts.TTNormsLightWOFF2}) format('woff2'), url(${fonts.TTNormsLightWOFF}) format('woff');
+  }
+
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: italic;
+    font-weight: 300;
+    font-display: auto;
+    src: local('TTNorms LightItalic'), local('TTNorms-LightItalic'), url(${fonts.TTNormsLightItalicWOFF2}) format('woff2'), url(${fonts.TTNormsLightItalicWOFF}) format('woff');
+  }
+
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: normal;
+    font-weight: normal;
+    font-display: auto;
+    src: local('TTNorms'), local('TTNorms-Regular'), url(${fonts.TTNormsRegularWOFF2}) format('woff2'), url(${fonts.TTNormsRegularWOFF}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: italic;
+    font-weight: normal;
+    font-display: auto;
+    src: local('TTNorms Italic'), local('TTNorms-Italic'), url(${fonts.TTNormsItalicWOFF2}) format('woff2'), url(${fonts.TTNormsItalicWOFF}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: normal;
+    font-weight: 500;
+    font-display: auto;
+    src: local('TTNorms Medium'), local('TTNorms-Medium'), url(${fonts.TTNormsMediumWOFF2}) format('woff2'), url(${fonts.TTNormsMediumWOFF}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: italic;
+    font-weight: 500;
+    font-display: auto;
+    src: local('TTNorms MediumItalic'), local('TTNorms-MediumItalic'), url(${fonts.TTNormsMediumItalicWOFF2}) format('woff2'), url(${fonts.TTNormsMediumItalicWOFF}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: normal;
+    font-weight: bold;
+    font-display: auto;
+    src: local('TTNorms Bold'), local('TTNorms-Bold'), url(${fonts.TTNormsBoldWOFF2}) format('woff2'), url(${fonts.TTNormsBoldWOFF}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: italic;
+    font-weight: bold;
+    font-display: auto;
+    src: local('TTNorms BoldItalic'), local('TTNorms-BoldItalic'), url(${fonts.TTNormsBoldItalicWOFF2}) format('woff2'), url(${fonts.TTNormsBoldItalicWOFF}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: normal;
+    font-weight: 800;
+    font-display: auto;
+    src: local('TTNorms ExtraBold'), local('TTNorms-ExtraBold'), url(${fonts.TTNormsExtraBoldWOFF2}) format('woff2'), url(${fonts.TTNormsExtraBoldWOFF}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: italic;
+    font-weight: 800;
+    font-display: auto;
+    src: local('TTNorms ExtraBoldItalic'), local('TTNorms-ExtraBoldItalic'), url(${fonts.TTNormsExtraBoldItalicWOFF2}) format('woff2'), url(${fonts.TTNormsExtraBoldItalicWOFF}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: normal;
+    font-weight: 900;
+    font-display: auto;
+    src: local('TTNorms Heavy'), local('TTNorms-Heavy'), url(${fonts.TTNormsHeavyWOFF2}) format('woff2'), url(${fonts.TTNormsHeavyWOFF}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: italic;
+    font-weight: 900;
+    font-display: auto;
+    src: local('TTNorms HeavyItalic'), local('TTNorms-HeavyItalic'), url(${fonts.TTNormsHeavyItalicWOFF2}) format('woff2'), url(${fonts.TTNormsHeavyItalicWOFF}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'TTNorms';
+    font-style: normal;
+    font-weight: 1000;
+    font-display: auto;
+    src: local('TTNorms Black'), local('TTNorms-Black'), url(${fonts.TTNormsBlackWOFF2}) format('woff2'), url(${fonts.TTNormsBlackWOFF}) format('woff');
+  }
+
   *, *:before, *:after {
     box-sizing: inherit;
   }
+
   body {
     background-color: ${colors['blue-black']};
-    font-family: 'TTNorms', 'Product Sans', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', sans-serif;
+    font-family: 'TTNorms', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', sans-serif;
+    font-weight: normal;
     text-rendering: optimizeLegibility;
     width: 100%;
     height: 100%;
