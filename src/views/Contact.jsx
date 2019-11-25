e6e4f02b58601c57d874052e0076a31ce4c85ecc
Brett Stevenson <bstevensondev@gmail.com>
2019-11-24 19:58:28 -0800

    Fix issue with internal link formatting

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -36,18 +36,18 @@ const Contact = (props) => {
   const { height, width } = useWindowDimensions()
   const factor = props.factor
   let offset = props.offset
-  if (width <= 500) {
+  if (width <= 500)
     offset += (offset * 0.04)
-  }
+
   return (
     <Content offset={offset} factor={factor} speed={0.55}>
       <Inner>
         <Cube color='purple' />
         <ContactTitle>Get in touch</ContactTitle>
         <ContactText>
-          <PageLink to='./contact'>Say Hello</PageLink> or find me on other platforms:{' '}
-          <a href='https://github.com/tterb' aria-label='GitHub' target='_blank'>GitHub</a> &{' '}
-          <a href='https://www.dribbble.com/tterb/' aria-label='Dribbble' target='_blank'>Dribbble</a>
+          <PageLink to='/contact'>Say Hello</PageLink> or find me on other platforms:{' '}
+          <a href='https://github.com/tterb' aria-label='GitHub' target='_blank' rel='noopener noreferrer'>GitHub</a> &{' '}
+          <a href='https://www.dribbble.com/tterb/' aria-label='Dribbble' target='_blank' rel='noopener noreferrer'>Dribbble</a>
         </ContactText>
       </Inner>
     </Content>
