5a20e236ec009e79af6f4406cf7ccd7744e98907
Brett Stevenson <bstevensondev@gmail.com>
2019-11-24 22:18:09 -0800

    Add 'aria-label' to internal-link component

--- src/components/PageLink.jsx
+++ src/components/PageLink.jsx
@@ -1,24 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { colors } from '../../tailwind'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import { colors } from '../../tailwind'
 
-const PageLink = ({ direction, duration, to, className, children }) => (
-  <>
-    <AniLink cover bg={colors['background-alt']} className={className}
-    to={to} direction={direction} duration={duration}>{ children }</AniLink>
-  </>
-)
+const PageLink = (props) => {
+  const label = props.label ? props.label : props.children
+  return (
+    <>
+      <AniLink cover
+        className={props.className}
+        bg={colors['background-alt']}
+        direction={props.direction}
+        duration={props.duration}
+        aria-label={label}
+        to={props.to}>
+        { props.children }
+      </AniLink>
+    </>
+  )
+}
 
 PageLink.defaultProps = {
   direction: 'left',
   duration: 1.15,
 }
 PageLink.propTypes = {
+  className: PropTypes.string,
   duration: PropTypes.number,
   direction: PropTypes.string,
-  className: PropTypes.string,
+  label: PropTypes.string,
   to: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
 }
 
 export default PageLink
