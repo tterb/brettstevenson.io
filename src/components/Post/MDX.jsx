152d9efd94d5ccdd4a6e2e53b972352acb0e25ce
Brett Stevenson <bstevensondev@gmail.com>
2019-11-25 00:12:50 -0800

    Fix MDX component order

--- src/components/Post/MDX.jsx
+++ src/components/Post/MDX.jsx
@@ -9,12 +9,38 @@ import { OrderedList, UnorderedList } from './List'
 import CodePre from './Pre'
 import CodeBlock from './Code'
 
-/**
- * MDXBody
- * Here we're applying "global" selectors to make sure we maintain an article
- * body type feel. We're also applying all the Prism selecotors and styles within
- * the MDXBody.
- */
+const PrismCSS = `
+  .prism-code {
+    background: hsl(275, 5%, 19%);
+    width: 95%;
+    border-radius: 5px;
+    margin: 0.5rem auto;
+    border-radius: 5px;
+    overflow: scroll;
+    .token-line {
+      border-left: 3px solid transparent;
+      & > span {
+      }
+    }
+    .number-line {
+      display: inline-block;
+      color: hsl(275, 5%, 70%);
+      width: 25px;
+      user-select: none;
+      opacity: 0.3;
+    }
+    .token-line.highlight-line {
+      background: hsl(275, 5%, 25%);
+      border-left: 3px solid hsl(275, 5%, 50%);
+      margin: 0 -2rem;
+      padding: 0 2rem;
+    }
+    .operator + .maybe-class-name {
+      color: #ffcf74 !important;
+    }
+  }
+`
+
 const MDXBody = styled.div`
   ${tw`relative flex flex-col justify-center z-10`}
   ${PrismCSS}
@@ -49,35 +75,3 @@ MDX.propTypes = {
 }
 
 export default MDX
-
-const PrismCSS = `
-  .prism-code {
-    background: hsl(275, 5%, 19%);
-    width: 95%;
-    border-radius: 5px;
-    margin: 0.5rem auto;
-    border-radius: 5px;
-    overflow: scroll;
-    .token-line {
-      border-left: 3px solid transparent;
-      & > span {
-      }
-    }
-    .number-line {
-      display: inline-block;
-      color: hsl(275, 5%, 70%);
-      width: 25px;
-      user-select: none;
-      opacity: 0.3;
-    }
-    .token-line.highlight-line {
-      background: hsl(275, 5%, 25%);
-      border-left: 3px solid hsl(275, 5%, 50%);
-      margin: 0 -2rem;
-      padding: 0 2rem;
-    }
-    .operator + .maybe-class-name {
-      color: #ffcf74 !important;
-    }
-  }
-`
