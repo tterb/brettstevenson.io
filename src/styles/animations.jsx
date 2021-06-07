7df51f28ad2176705e4360b7fc8b9899f01484d7
Brett Stevenson <bstevensondev@gmail.com>
2021-06-06 23:02:22 -0700

    Remove unnecessary styles

--- src/styles/animations.jsx
+++ src/styles/animations.jsx
@@ -1,14 +1,5 @@
-import tw from 'tailwind.macro'
 import styled, { keyframes, css } from 'styled-components'
 
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`
 
 const wave = keyframes`
   0% {
@@ -21,47 +12,38 @@ const wave = keyframes`
     d: path("M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z");
   }
 `
-
-const upDown = keyframes`
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(30px);
-  }
-`
-
-const upDownWide = keyframes`
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(150px);
-  }
-`
-
-const upDownAnimation = css`
-  ${upDown} 4s ease-in-out infinite alternate;
-`
-
-const upDownWideAnimation = css`
-  ${upDownWide} 15s ease-in-out infinite alternate;
-`
-
-export const UpDown = styled.div`
-  ${tw`pin absolute`}
-  animation: ${upDownAnimation};
-`
-
-export const UpDownWide = styled.div`
-  ${tw`pin absolute`}
-  animation: ${upDownWideAnimation};
-`
-
 export const waveAnimation = length => css`
   animation: ${wave} ${length} linear infinite alternate;
 `
 
-export const rotateAnimation = length => css`
-  animation: ${rotate} ${length} linear infinite;
+const glitch = keyframes`
+  0%   { clip: rect( 31px, 9999px,  94px, 0) }
+  10%  { clip: rect(112px, 9999px, 156px, 0) }
+  20%  { clip: rect( 65px, 9999px,  77px, 0) }
+  30%  { clip: rect(257px, 9999px, 290px, 0) }
+  40%  { clip: rect( 64px, 9999px, 138px, 0) }
+  50%  { clip: rect( 31px, 9999px,  55px, 0) }
+  60%  { clip: rect( 59px, 9999px,  84px, 0) }
+  70%  { clip: rect( 34px, 9999px, 115px, 0) }
+  80%  { clip: rect(193px, 9999px, 276px, 0) }
+  90%  { clip: rect( 98px, 9999px, 129px, 0) }
+  100% { clip: rect(202px, 9999px, 284px, 0) }
+`
+export const glitchAnimation = length => css`
+  animation: ${glitch} ${length} infinite linear alternate-reverse;
+`
+
+const glitchHover = keyframes`
+  0%  { transform: translate(0) }
+  20% {
+    transform: translate(-5px, 5px);
+    transform: skewX(10deg);
+  }
+  40% { transform: translate(-5px, -5px); }
+  60% { transform: translate(5px, 5px) }
+  80% { transform: translate(5px, -5px) }
+  to  { transform: translate(0) }
+`
+export const glitchHoverAnimation = (length, delay, option) => css`
+  animation: ${glitchHover} ${length} ${delay} cubic-bezier(0.25, 0.46, 0.45, 0.94) ${option} both infinite;
 `
