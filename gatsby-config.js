4d2a9942a59e74aaf9c36c847db8a8114e929ab8
Brett Stevenson <bstevensondev@gmail.com>
2021-10-06 12:43:09 -0700

    Remove gatsby-plugin-react-axe for production bundle size

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
     { resolve: `gatsby-plugin-react-helmet` },
     { resolve: `gatsby-plugin-sitemap` },
     { resolve: `gatsby-plugin-styled-components` },
-    { resolve: `gatsby-plugin-react-axe` },
+    { resolve: `gatsby-plugin-webpack-bundle-analyser-v2` },
     {
       resolve: `gatsby-plugin-image`,
       options: {
