47615055e138850e8e3600e279736e2ff98b68dd
Brett Stevenson <bstevensondev@gmail.com>
2023-01-29 16:59:11 -0800

    Replace deprecated dependencies

--- src/components/Typed.jsx
+++ src/components/Typed.jsx
@@ -8,36 +8,9 @@ export const Typed = (props) => {
   // Create reference to store the Typed instance itself
 	const typed = React.useRef(null);
 
-  // useEffect(() => {
-  //   if (!ref.current) {
-  //     return;
-  //   }
-  //   const typed = new TypedLib(ref.current, options);
-
-  //   return () => {
-  //     if (!typed) {
-  //       return;
-  //     }
-  //     typed.destroy();
-  //   };
-  // }, [ref, options]);
-
   useEffect(() => {
-    // const options = {
-    // 	strings: [
-    //     'Some <i>strings</i> are slanted',
-    //     'Some <strong>strings</strong> are bold',
-    //     'HTML characters &times; &copy;'
-    //   ],
-    //   typeSpeed: 50,
-    //   backSpeed: 50,
-    // };
-
     // elRef refers to the <span> rendered below
-    // debugger;
-    // if (props.strings != null || props.strings.length !== 0) {
     typed.current = new TypedLib(el.current, props);
-    // }
 
     return () => {
       // Make sure to destroy Typed instance during cleanup
